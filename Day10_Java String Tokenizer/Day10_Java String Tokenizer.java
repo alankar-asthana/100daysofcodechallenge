6072e5030839247858f836c91ff3712999414c39
alankar21asthana <61406986+alankar21asthana@users.noreply.github.com>
2022-11-15 17:45:10 +0530

    Create Day10_Java String Tokenizer.java

---
+++ Day10_Java String Tokenizer/Day10_Java String Tokenizer.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        StringTokenizer st = new StringTokenizer(s, " !,?._'@");
+        System.out.println(st.countTokens());
+        while (st.hasMoreTokens()) {
+            System.out.println(st.nextToken());
+        }
+        scan.close();
+    }
+}
