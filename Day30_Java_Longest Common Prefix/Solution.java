1981b519e127450cbf6655545d16796a12916a91
Alankar Asthana <61406986+alankar21asthana@users.noreply.github.com>
2023-01-02 21:37:35 +0530

    Create Solution.java

---
+++ Day30_Java_Longest Common Prefix/Solution.java
@@ -0,0 +1,13 @@
+class Solution{
+    public String longestCommonPrefix(String[] S){
+        if(S.length==0) return "";
+        String prefix=S[0];
+        for(int i=1;i<S.length;i++){
+            while(S[i].indexOf(prefix)!=0){
+                prefix=prefix.substring(0,prefix.length()-1);
+                if(prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+}
