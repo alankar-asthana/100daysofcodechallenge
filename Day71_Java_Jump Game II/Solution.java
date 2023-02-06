f4eacc6da6393bf85a07a768aef6a7932f15a481
Alankar Asthana <61406986+alankar21asthana@users.noreply.github.com>
2023-02-06 23:48:42 +0530

    Create Solution.java

---
+++ Day71_Java_Jump Game II/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        int count = 0;
+        int end = 0;
+        int farthest = 0;
+        for (int i = 0; i < nums.length - 1; ++i) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (farthest >= nums.length - 1) {
+                ++count;
+                break;
+            }
+            if (i == end) {
+                ++count;
+                end = farthest;
+            }
+        }
+        return count;
+    }
+}
